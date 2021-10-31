162ed52f9f0c97119de10e8a9de5ee149c4ff235
SampabloC <sampablo-jisc@outlook.com>
2021-10-31 00:44:27 -0500

    Function to create new task

--- todo-app/src/components/TodosContainer.js
+++ todo-app/src/components/TodosContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import update from "immutability-helper";
 
 class TodosContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      todos: []
+      todos: [],
+      inputValue: "",
     }
   }
 
@@ -21,13 +23,36 @@ class TodosContainer extends Component {
     this.getTodos()
   }
 
+  createTodo = (input) => {
+    if(input.key === "Enter" && !(input.target.value === "")) {
+      axios.post("/api/v1/todos", { todo: { title: input.target.value, done: false} })
+      .then( (response) => {
+        const todos = update(this.state.todos, {
+          $splice: [[0, 0, response.data]],
+        });
+
+        this.setState( {
+          todos: todos,
+          inputValue: "",
+        });
+      })
+      .catch((error) => console.log(error));
+    }
+  };
+
+  handleChange = (input) => {
+    this.setState( { inputValue: input.target.value });
+  };
+
   render() {
     return (
       <div>
         <div className="inputContainer">
           <input className="taskInput" type="text" 
             placeholder="Add a task" maxLength="50"
-            onKeyPress={this.createTodo} />
+            onKeyPress={this.createTodo}
+            value={this.state.inputValue}
+            onChange={this.handleChange} />
         </div>  	    
 	<div className="listWrapper">
 	   <ul className="taskList">
